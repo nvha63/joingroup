d97e52d998078a55494121f343c38895a9fbbd35
nghia <trungnghiaabcd1234@gmail.com>
2019-08-28 14:24:15 +0700

    nghia

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan'); 
+var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/admin');
+//var http = require('http');
+//var io = require('socket.io')(http);
+var app = express();
+var socket_io = require("socket.io");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+// Socket.io
+var io = socket_io();
+app.io = io;
+// socket.io events
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+  });
+});
+module.exports = app;
